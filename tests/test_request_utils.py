494941352c5d749fbcca3c0e2038154c4cff162a
SteelPh0enix <steelphoenix2137@gmail.com>
2022-11-24 20:12:06 +0100

    code reformat

--- tests/test_request_utils.py
+++ tests/test_request_utils.py
@@ -1,12 +1,13 @@
 import pypoeninja.request_utils as request
 
-TEST_API_URL = 'https://jsonplaceholder.typicode.com/todos/1'
+TEST_API_URL = "https://jsonplaceholder.typicode.com/todos/1"
 EXPECTED_RESPONSE = {
-  "userId": 1,
-  "id": 1,
-  "title": "delectus aut autem",
-  "completed": False
+    "userId": 1,
+    "id": 1,
+    "title": "delectus aut autem",
+    "completed": False,
 }
+
 
 def test_cache_enabling():
     # Cache should be enabled by default
@@ -15,6 +16,7 @@ def test_cache_enabling():
     assert not request.is_cache_enabled()
     request.enable_cache()
     assert request.is_cache_enabled()
+
 
 def test_getting_json():
     json = request.get_json(TEST_API_URL)
