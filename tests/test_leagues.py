02563ca9288a38e953af0df37c99e6e9311417ef
SteelPh0enix <steelphoenix2137@gmail.com>
2022-11-26 19:14:25 +0100

    removed unnecesary verbosity from tests and not needed constants

--- tests/test_leagues.py
+++ tests/test_leagues.py
@@ -1,23 +1,11 @@
 import pypoeninja.leagues as leagues
 from pypoeninja.leagues_constants import (
-    HARDCORE_LEAGUE_DISPLAY_NAME,
     HARDCORE_LEAGUE_NAME,
-    HARDCORE_LEAGUE_URL,
-    STANDARD_LEAGUE_DISPLAY_NAME,
     STANDARD_LEAGUE_NAME,
-    STANDARD_LEAGUE_URL,
-    TEMPORARY_LEAGUE_DISPLAY_NAME,
-    TEMPORARY_LEAGUE_HC_DISPLAY_NAME,
     TEMPORARY_LEAGUE_HC_NAME,
-    TEMPORARY_LEAGUE_HC_SSF_DISPLAY_NAME,
     TEMPORARY_LEAGUE_HC_SSF_NAME,
-    TEMPORARY_LEAGUE_HC_SSF_URL,
-    TEMPORARY_LEAGUE_HC_URL,
     TEMPORARY_LEAGUE_NAME,
-    TEMPORARY_LEAGUE_SSF_DISPLAY_NAME,
     TEMPORARY_LEAGUE_SSF_NAME,
-    TEMPORARY_LEAGUE_SSF_URL,
-    TEMPORARY_LEAGUE_URL,
 )
 
 EXPECTED_ECONOMY_LEAGUES = [
@@ -56,31 +44,6 @@ def test_fetch_general_metadata_returns_expected_economy_leagues():
     assert found_leagues == len(EXPECTED_ECONOMY_LEAGUES)
 
 
-def test_fetch_general_metadata_economy_leagues_have_expected_field_values():
-    meta = leagues.fetch_general_metadata()
-    for league in meta.economyLeagues:
-        if league.name == TEMPORARY_LEAGUE_NAME:
-            assert league.url == TEMPORARY_LEAGUE_URL
-            assert league.displayName == TEMPORARY_LEAGUE_DISPLAY_NAME
-            assert league.hardcore is False
-            assert league.indexed is True
-        if league.name == TEMPORARY_LEAGUE_HC_NAME:
-            assert league.url == TEMPORARY_LEAGUE_HC_URL
-            assert league.displayName == TEMPORARY_LEAGUE_HC_DISPLAY_NAME
-            assert league.hardcore is True
-            assert league.indexed is False
-        elif league.name == STANDARD_LEAGUE_NAME:
-            assert league.url == STANDARD_LEAGUE_URL
-            assert league.displayName == STANDARD_LEAGUE_DISPLAY_NAME
-            assert league.hardcore is False
-            assert league.indexed is False
-        elif league.name == HARDCORE_LEAGUE_NAME:
-            assert league.url == HARDCORE_LEAGUE_URL
-            assert league.displayName == HARDCORE_LEAGUE_DISPLAY_NAME
-            assert league.hardcore is True
-            assert league.indexed is False
-
-
 def test_fetch_general_metadata_returns_expected_build_leagues():
     meta = leagues.fetch_general_metadata()
     found_leagues = 0
@@ -91,31 +54,6 @@ def test_fetch_general_metadata_returns_expected_build_leagues():
     assert found_leagues == len(EXPECTED_BUILD_LEAGUES)
 
 
-def test_fetch_general_metadata_build_leagues_have_expected_field_values():
-    meta = leagues.fetch_general_metadata()
-    for league in meta.economyLeagues:
-        if league.name == TEMPORARY_LEAGUE_NAME:
-            assert league.url == TEMPORARY_LEAGUE_URL
-            assert league.displayName == TEMPORARY_LEAGUE_DISPLAY_NAME
-            assert league.hardcore is False
-            assert league.indexed is True
-        if league.name == TEMPORARY_LEAGUE_HC_NAME:
-            assert league.url == TEMPORARY_LEAGUE_HC_URL
-            assert league.displayName == TEMPORARY_LEAGUE_HC_DISPLAY_NAME
-            assert league.hardcore is True
-            assert league.indexed is False
-        if league.name == TEMPORARY_LEAGUE_SSF_NAME:
-            assert league.url == TEMPORARY_LEAGUE_SSF_URL
-            assert league.displayName == TEMPORARY_LEAGUE_SSF_DISPLAY_NAME
-            assert league.hardcore is False
-            assert league.indexed is False
-        if league.name == TEMPORARY_LEAGUE_HC_SSF_NAME:
-            assert league.url == TEMPORARY_LEAGUE_HC_SSF_URL
-            assert league.displayName == TEMPORARY_LEAGUE_HC_SSF_DISPLAY_NAME
-            assert league.hardcore is True
-            assert league.indexed is False
-
-
 def test_fetch_league_metadata_returns_none_on_invalid_league():
     assert leagues.fetch_league_metadata(INVALID_LEAGUE_NAME) is None
 
@@ -124,3 +62,8 @@ def test_fetch_league_metadata_returns_expected_leagues():
     for league_name in EXPECTED_LEAGUES:
         league = leagues.fetch_league_metadata(league_name)
         assert league is not None
+
+
+def test_fetch_current_temporary_league_metadata_returns_metadata():
+    """this verifies that cast() is not silencing potential error, although other tests should also catch it"""
+    assert leagues.fetch_current_temporary_league_metadata() is not None
